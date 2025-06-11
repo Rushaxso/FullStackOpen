5c730ad6516f9300fb4457bfe93bc47b03024913
Iivari Hautala <iivari.hautala@aalto.fi>
2025-06-11 21:04:44 +0300

    removed original const definitions

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,12 +32,6 @@ const App = () => {
     {name: 'Using Props to pass data', exercises: 7},
     {name: 'State of a Component', exercises: 14}
   ]
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
