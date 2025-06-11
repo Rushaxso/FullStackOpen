5860c762c8f901937ae145b9edeba40a51609b63
Iivari Hautala <iivari.hautala@aalto.fi>
2025-06-11 21:01:35 +0300

    exercises 1.1 and 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+const Content = ({parts}) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises}/>
+      <Part name={parts[1].name} exercises={parts[1].exercises}/>
+      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using Props to pass data', exercises: 7},
+    {name: 'State of a Component', exercises: 14}
+  ]
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
