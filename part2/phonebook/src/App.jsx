d6865a43e26339808fdb4865055391a4dd520b40
Rushaxso <iivari.hautala1@gmail.com>
2025-06-18 18:14:47 +0300

    laptop test

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -46,7 +46,8 @@ const App = () => {
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    { name: 'Testi Tornado', number: '040-987654321'}
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
