468829d9a0af0789482cc5c5f2b0b748a3df2077
Rushaxso <iivari.hautala1@gmail.com>
2025-07-23 19:03:56 +0300

    exercise 5.12 blog list frontend step 12

--- part5/blogList/frontend/src/components/Blog.jsx
+++ part5/blogList/frontend/src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const Blog = ({ blog, updateBlog, removeBlog, user }) => {
   const blogStyle = {
@@ -34,6 +35,13 @@ const Blog = ({ blog, updateBlog, removeBlog, user }) => {
       </div>
   </div>
   )
+}
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  updateBlog: PropTypes.func.isRequired,
+  removeBlog: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired
 }
 
 export default Blog
